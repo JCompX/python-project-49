4aa7f425a141384de7672730d476b27c530614c6
JCompX <freelansermail@gmail.com>
2023-01-24 23:08:11 +0300

    added brain_even and brain_calc games

---
+++ brain_games/games/logic_brain_calc.py
@@ -0,0 +1,21 @@
+import random
+
+
+def game_question():
+    return 'What is the result of the expression?'
+
+
+def data_question():
+    int1 = random.randint(30, 70)
+    int2 = random.randint(1, 30)
+    chosen_operand = random.choice(['+', '-', '*'])
+    return f'{int1} {chosen_operand} {int2}'
+
+
+def true_answer(data_question):
+    return eval(data_question)
+
+
+def check_user_answer(data_question, user_answer):
+    true_answer = eval(data_question)
+    return True if int(user_answer) == true_answer else False
